1465dd0b7456417e6fd0120169c8e9be4f65b28e
ABC <mayukc55@gmail.com>
2023-03-30 00:37:51 +0530

    day 27 done

---
+++ src/day27.java
@@ -0,0 +1,18 @@
+
+//For the given input number calculate the double of it without using arithmetic operator.
+
+//Input
+//4
+
+//Output
+//8
+
+import  java.util.*;
+public class day27 {
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+        System.out.println("enter your number : ");
+        int num=sc.nextInt();
+        System.out.println(num<<1);
+    }
+}
