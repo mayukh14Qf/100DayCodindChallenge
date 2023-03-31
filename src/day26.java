168ceef6bfc6e3839c4d83a6b11d308f1f43e30d
ABC <mayukc55@gmail.com>
2023-03-31 17:58:28 +0530

    day 29 done

--- src/day26.java
+++ src/day26.java
@@ -4,7 +4,7 @@ Get the number of people in the room as input from the user. Then calculate the 
 
         For e.g. If there are N people in the room then the first person has to shake hand with N-1 people and second person has to shake hand with N-1-1 people i.e., N-2 handshakes are possible. Thus, it goes on.
 
-        So total hand shakes = N-1 + N-2 + N-3 +………+1 + 0                                                                  */
+        So total hand shakes = N-1 + N-2 + N-3 +………+1 + 0                                                                                                                                                     */
 
 
 import java.util.*;
---
+++ src/day29.java
@@ -0,0 +1,15 @@
+
+
+//Get two strings as input from the user and then concatenate it.
+
+import java.util.*;
+public class day29 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter 1st String ");
+        String a=sc.nextLine();
+        System.out.println("enter your 2nd String");
+        String b= sc.nextLine();
+        System.out.println(a+b);
+    }
+}
