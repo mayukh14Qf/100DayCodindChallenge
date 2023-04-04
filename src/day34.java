233748182ba699d59191fa2f3241c5c00030fdc6
ABC <mayukc55@gmail.com>
2023-04-04 19:13:51 +0530

    day 34 done

---
+++ src/day34.java
@@ -0,0 +1,29 @@
+/*
+Get an algebraic expression as input from the user and then remove all the brackets in that.
+
+Input
+7x+(2*y)
+
+Output
+7x+2*y
+
+ */
+
+import java.util.Scanner;
+public class day34 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your Equation");
+        String s=sc.nextLine();
+        StringBuilder sb= new StringBuilder("");
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)==')' || s.charAt(i)=='{' || s.charAt(i)=='}' || s.charAt(i)=='[' || s.charAt(i)==']'){
+                continue;
+            }
+            else {
+                sb.append(s.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
