7525e842eb84dbe249b0c259cc4300f04b3da2ab
ABC <mayukc55@gmail.com>
2023-04-14 20:30:39 +0530

    day 43 done

---
+++ src/day43.java
@@ -0,0 +1,55 @@
+
+/*
+Get an array as input from the user and check the type of the array, whether it is odd, even or mixed type.
+
+Input
+Enter size of array :
+3
+
+Enter elements
+1 3 5
+
+Output
+Odd
+
+ */
+
+
+
+import java.util.Scanner;
+public class day43 {
+    public static String check(int [] arr){
+        int flag=0;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                continue;
+            }
+            else {
+                flag=1;
+            }
+            while (i<arr.length-1){
+                if ((arr[i]%2==0 && arr[i+1]%2!=0 ) || (arr[i]%2!=0 && arr[i+1]%2==0 )) {
+                return "mixed";
+                }
+                i++;
+            }
+        }
+        if (flag==1) {
+            return "odd";
+        }
+        else {
+            return "even";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of array :");
+        int size=sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter elements ");
+        for (int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(check(arr));
+    }
+}
